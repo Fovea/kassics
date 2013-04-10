cfc957b5bd4a9d0de8c84e8b575523880a328c3e
Jean-Christophe Hoelt <hoelt@fovea.cc>
2013-04-10 20:23:25 +0300

    Optimize, get rid of jQuery

--- kassics.js
+++ kassics.js
@@ -26,6 +26,103 @@
     // Keep in sync with package.json and readme
     Kassics.VERSION = "0.0.2";
 
+    // Quality of effects
+    // Adjust per device
+    Kassics.qualityCoef = 1.0;
+
+    // Animations scheduler.
+    // constructor
+    var Scheduler = function () {
+        this.animations = {};
+        this.nextAnimationId = 0;
+        this.lastframe = 0;
+
+        this.requestAnimationFrame = window.requestAnimationFrame ||
+            window.mozRequestAnimationFrame ||
+            window.webkitRequestAnimationFrame ||
+            window.msRequestAnimationFrame;
+
+        this.cancelAnimationFrame = window.cancelAnimationFrame ||
+            window.mozCancelAnimationFrame;
+
+    };
+
+    _.extend(Scheduler.prototype, {
+
+        // Perform the animations
+        idle: function (timestamp) {
+
+            // Calculate delta since last frame
+            var dt = timestamp - this.lastframe;
+            if (this.lastframe === 0)
+                dt = 0;
+            else if (dt > 200)
+                dt = 200;
+            this.lastframe = timestamp;
+
+            // console.log('frame(t=' + timestamp + ', dt=' + dt + ')');
+
+            // Call idle on all animations
+            _.each(this.animations, function (a) {
+                a.idle(timestamp, dt);
+            });
+        },
+
+        // Add an animation, return an animationID
+        add: function (animation) {
+            var cid = ++this.nextAnimationId;
+            this.animations[cid] = animation;
+            return cid;
+        },
+
+        // Clean the animation from id
+        remove: function (animationID) {
+            delete this.animations[cid];
+        },
+
+        // Starts the scheduling of animations.
+        start: function () {
+
+            // Check if the scheduler is already running, do nothing if so.
+            if (this.started) return;
+            this.started = true;
+            var that = this;
+
+            // If requestAnimationFrame is supported, that's the way to go.
+            // reference to the appropriate requestAnimationFrame has been saved
+            // in constructor.
+            if (this.requestAnimationFrame) {
+                function frame(timestamp) {
+                    that.idle(timestamp);
+                    if (that.started) {
+                        that.requestID = that.requestAnimationFrame.call(window, frame);
+                    }
+                };
+                this.requestID = that.requestAnimationFrame.call(window, frame);
+            }
+            else {
+                // Fallback to setInterval.
+                this.intervalID = window.setInterval(17, function () {
+                    var timestamp = +new Date();
+                    idle(timestamp);
+                });
+            }
+        },
+
+        // Stop running the animations.
+        stop: function () {
+            this.started = false;
+            if (this.requestID) {
+                this.cancelAnimationFrame(this.requestID);
+                delete this.requestID;
+            }
+            if (this.intervalID) {
+                window.clearInterval(this.intervalID);
+                delete this.intervalID;
+            }
+        }
+    });
+
     // TODO: get rid of JQuery and Underscore, use something like this:
     //
     // mini-pico-tiny convenience micro-framework, ymmv
@@ -65,43 +162,90 @@
 
     // Translate an image.
     var k6position = function (x, y) {
-        this.css(cssTransform, 'translate3d(' + x + 'px,' + y + 'px,0)');
-        this.attr('k6x', x);
-        this.attr('k6y', y);
+        // this.setAttribute('k6x', x);
+        // this.setAttribute('k6y', y);
+        this.k6x = x;
+        this.k6y = y;
+        this.style.webkitTransform = 'translate3d(' + x + 'px,' + y + 'px,0)';
     };
 
     // Resize an image.
     var k6size = function (w, h) {
-        this.css('width', w);
-        this.css('height', h);
+        this.style.width = w + 'px';
+        this.style.height = h + 'px';
     };
 
     // Change layer for an image.
     var k6layer = function (layer) {
-        this.css('z-index', layer);
-    };
-
-    // Set image draggable status.
-    var k6draggable = function (state) {
-        var oldDraggable = this.attr('k6drag');
-        var newDraggable = state ? 'true' : 'false';
-        if (oldDraggable !== newDraggable) {
-            this.attr('k6drag', newDraggable);
-            this.kassics.stopDragging(this);
-        }
+        this.style.zIndex = layer;
     };
 
     // Change image opacity.
     var k6opacity = function (opacity) {
-        this.css('opacity', opacity);
+        this.style.opacity = opacity;
     };
+
+    var k6update = function (p) {
+        this.style.width = p.k6w + 'px';
+        this.style.height = p.k6h + 'px';
+        this.style.opacity = p.k6a;
+        this.style.webkitTransform = 'translate3d(' + p.k6x + 'px,' + p.k6y + 'px,0)';
+    };
+
+    // Set image draggable status.
+    var k6draggable = function (state) {
+        var oldDraggable = this.getAttribute('k6drag');
+        var newDraggable = state ? 'true' : 'false';
+        if (oldDraggable !== newDraggable) {
+            this.getAttribute('k6drag', newDraggable);
+            this.k6stage.stopDragging(this);
+        }
+    };
+
+    // Adds Kassics API to a DOM element.
+    var extendDOM = function (stage, el) {
+        // Setters
+        el.k6position = k6position;
+        el.k6size = k6size;
+        el.k6layer = k6layer;
+        el.k6draggable = k6draggable;
+        el.k6opacity = k6opacity;
+        el.k6update = k6update;
+        el.k6stage = stage;
+        el.k6listeners = {};
+
+        // Events API
+        el.k6on = function (event, callback) {
+            this.k6listeners[event] = callback;
+        };
+        el.k6off = function (event) {
+            delete this.k6listeners[event];
+        };
+        el.k6trigger = function (event, data) {
+            var callback = this.k6listeners[event];
+            if (callback)
+                callback(data);
+        };
+
+        // Remove from Stage
+        el.k6remove = function () {
+            this.parentNode.removeChild(this);
+        };
+    }
+
 
     // Kassics, a 2D drawing area.
     var Stage = Kassics.Stage = function (options) {
         this.options = options;
-        var images = this.images = {};
-        this.setElement(options.el || $('<div />'));
+        var el = options.el;
+        while (el && typeof el.length !== 'undefined') {
+            el = el[0];
+        }
+        this.setElement(el || new Div());
         this.touches = {};
+        this.scheduler = new Scheduler();
+        this.scheduler.start();
+        extendDOM(this, el);
     };
 
     _.extend(Stage.prototype, {
@@ -109,68 +253,45 @@
         // Change the element to render to.
         setElement: function (el) {
             this.el = el;
-            this.$el = el;
+            this.$el = $(el);
             this._configure();
         },
 
         // Configure our DOM element
         _configure: function () {
-            this.$el.css('width', this.options.width || 100);
-            this.$el.css('height', this.options.height || 100);
-            this.$el.css('overflow', 'hidden');
-            this.$el.css('display', 'block');
-            this.$el.css('margin', '0');
-            this.$el.css('padding', '0');
+            this.el.style.width    = (this.options.width || 128) + 'px';
+            this.el.style.height   = (this.options.height || 128) + 'px';
+            this.el.style.overflow = 'hidden';
+            this.el.style.display  = 'block';
+            this.el.style.margin   = '0';
+            this.el.style.padding  = '0';
         },
 
         // Add an image to the stage
         // returns a Kassics.Image.
         add: function (options) {
-            var $image = $(options.image);
+            var image = options.image;
 
             // Generate a CID, store it in the element.
             var cid = _.uniqueId('k6_');
-            $image.attr('k6cid', cid);
+            image.id = cid;
 
             // Make it a 'floating' image.
-            $image.css('display', 'block');
-            $image.css('position', 'absolute');
-            $image.css('left', 0);
-            $image.css('top', 0);
-            this._extendImage($image);
+            image.style.display = 'block';
+            image.style.position = 'absolute';
+            image.style.left = 0;
+            image.style.top = 0;
+            extendDOM(this, image);
 
             // Set initial values from options.
-            $image.k6layer(options.layer);
-            $image.k6position(options.x, options.y);
-            $image.k6size(options.width, options.height);
-            $image.k6draggable(options.draggable);
-
-            // Add to the registry
-            this.images[cid] = $image;
+            image.k6layer(options.layer);
+            image.k6position(options.x, options.y);
+            image.k6size(options.width, options.height);
+            image.k6draggable(options.draggable);
 
             // Append and return the image.
-            this.$el.append($image);
-            return $image;
-        },
-
-        // Direct access to Kassics API from the image.
-        _extendImage: function ($image) {
-            var that = this;
-
-            // Setters
-            $image.k6position = k6position;
-            $image.k6size = k6size;
-            $image.k6layer = k6layer;
-            $image.k6draggable = k6draggable;
-            $image.k6opacity = k6opacity;
-            $image.kassics = this;
-
-            // Remove from Stage
-            $image.k6remove = function () {
-                var cid = this.attr('k6cid');
-                delete that.images[cid];
-                this.remove();
-            }
+            this.el.appendChild(image);
+            return image;
         },
 
         bindEvents: function () {
@@ -196,10 +317,10 @@
             this.$el.off('mouseup');
         },
 
-        stopDragging: function ($image) {
+        stopDragging: function (image) {
             for (var identifier in this.touches) {
                 var touch = this.touches[identifier];
-                if (touch.$drag === $image) {
+                if (touch.drag === image) {
                     delete this.touches[identifier];
                     return;
                 }
@@ -208,29 +329,29 @@
 
         _dragStart: function (t) {
             // Draggable target.
-            if (t.$target.attr('k6drag') === 'true') {
-                t.$drag = this.images[t.$target.attr('k6cid')];
+            if (t.target.getAttribute('k6drag') === 'true') {
+                t.drag = t.target;
                 t.dragStartX = t.x;
                 t.dragStartY = t.y;
-                t.dragTargetX = +t.$target.attr('k6x');
-                t.dragTargetY = +t.$target.attr('k6y');
-                t.$drag.trigger('k6dragstart', {target:t.$drag, x:t.dragTargetY, y:t.dragTargetY});
+                t.dragTargetX = +t.target.getAttribute('k6x');
+                t.dragTargetY = +t.target.getAttribute('k6y');
+                t.drag.k6trigger('dragstart', {target:t.drag, x:t.dragTargetY, y:t.dragTargetY});
             }
         },
         _dragMove: function (t) {
-            if (t.$drag) {
+            if (t.drag) {
                 var newX = t.x - t.dragStartX + t.dragTargetX;
                 var newY = t.y - t.dragStartY + t.dragTargetY;
-                t.$drag.k6position(newX, newY);
-                t.$drag.trigger('k6dragmove', {target:t.$drag, x:newX, y:newY});
+                t.drag.k6position(newX, newY);
+                t.drag.k6trigger('dragmove', {target:t.drag, x:newX, y:newY});
             }
         },
         _dragEnd: function (t) {
-            if (t.$drag) {
+            if (t.drag) {
                 var newX = t.x - t.dragStartX + t.dragTargetX;
                 var newY = t.y - t.dragStartY + t.dragTargetY;
-                t.$drag.k6position(newX, newY);
-                t.$drag.trigger('k6dragend', {target:t.$drag, x:newX, y:newY});
+                t.drag.k6position(newX, newY);
+                t.drag.k6trigger('dragend', {target:t.drag, x:newX, y:newY});
             }
         },
 
@@ -238,7 +359,7 @@
             e.preventDefault();
             e.stopPropagation();
             var identifier = -(1 + e.which);
-            var t = {x: e.clientX, y: e.clientY, $target: $(e.target)};
+            var t = {x: e.clientX, y: e.clientY, target: e.target};
             this._dragStart(t);
             this.touches[identifier] = t;
             return false;
@@ -273,7 +394,7 @@
             var e = event.originalEvent;
             for (var i in e.changedTouches) {
                 var touch = e.changedTouches[i];
-                var t = {x: touch.pageX, y: touch.pageY, $target: $(touch.target)};
+                var t = {x: touch.pageX, y: touch.pageY, target: touch.target};
                 this._dragStart(t);
                 this.touches[touch.identifier] = t;
             }
